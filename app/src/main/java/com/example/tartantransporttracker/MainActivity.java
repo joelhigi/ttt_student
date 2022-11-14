5d8f576bb4111aeea9507b31d03e237a9ab3483b
edgar <shumbushoedgar@gmail.com>
2022-11-14 11:27:16 +0200

    header bar

--- app/src/main/java/com/example/tartantransporttracker/MainActivity.java
+++ app/src/main/java/com/example/tartantransporttracker/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.tartantransporttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.
     }
 }
