9d680713ec66e39e24812802fb1087459390ad09
edgar <shumbushoedgar@gmail.com>
2022-11-23 15:56:21 +0200

    added signup fragment

--- app/src/main/java/com/example/tartantransporttracker/LoginActivity.java
+++ app/src/main/java/com/example/tartantransporttracker/LoginActivity.java
@@ -1,16 +1,30 @@
 package com.example.tartantransporttracker;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class LoginActivity extends AppCompatActivity {
     ViewPager viewPager;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.auth_tabLayout);
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
     }
 }
