0090c8cf39abd85b374bc6bc9cd55ba833b4af24
unknown <diddynu2000@gmail.com>
2022-12-06 19:54:20 +0200

    adding roles and crud for routes

--- app/src/main/java/com/tartantransporttracker/managers/RouteManager.java
+++ app/src/main/java/com/tartantransporttracker/managers/RouteManager.java
@@ -1,4 +1,32 @@
 package com.tartantransporttracker.managers;
 
+import com.tartantransporttracker.models.Route;
+import com.tartantransporttracker.repository.RouteRepository;
+import com.tartantransporttracker.repository.UserRepository;
+
 public class RouteManager {
+    private static volatile RouteManager instance;
+    private RouteRepository routeRepository;
+
+    public RouteManager() {
+        routeRepository = routeRepository.getInstance();
+    }
+
+    public  static RouteManager getInstance(){
+        RouteManager result = instance;
+        if(result != null){
+            return result;
+        }
+        synchronized (UserRepository.class){
+            if(instance == null){
+                instance = new RouteManager();
+            }
+            return instance;
+        }
+    }
+
+    public void createRoute(Route route){
+        routeRepository.createRoute(route);
+    }
+
 }
