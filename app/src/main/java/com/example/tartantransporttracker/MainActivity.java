9049d613cf7d1edde607747277f9f663775dc770
Joel Higiro <joelhigi@andrew.cmu.edu>
2022-12-06 14:30:44 +0200

    nav menu

--- app/src/main/java/com/example/tartantransporttracker/MainActivity.java
+++ app/src/main/java/com/example/tartantransporttracker/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.tartantransporttracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
+import android.view.MenuItem;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawLayout);
+
+        navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.logout_button);
+
         btn = findViewById(R.id.goToLogin);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +39,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logout_button:
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                break;
+
+                //TODO: Dear Special Ops, Any other activities can be added below for easy navigation
+            //TODO: Just remember to use the switch below. Tie the ID to a menu item.
+//            case R.id.scans:
+//                Intent intent = new Intent(MainActivity.this,PastRecords.class);
+//                startActivity(intent);
+//                break;
+//
+//            case R.id.diseases:
+//                Intent intent1 = new Intent(MainActivity.this,MaizeDiseases.class);
+//                startActivity(intent1);
+//                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
